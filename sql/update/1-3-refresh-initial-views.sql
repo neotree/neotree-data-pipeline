5017fb0672e939e08a09155b6af2ebb7e49204ce
yalisassoon <yali@snowplowanalytics.com>
2020-03-13 16:36:46 +0000

    Restructure for maintenance

---
+++ sql/update/1-3-refresh-initial-views.sql
@@ -0,0 +1,7 @@
+refresh materialized view scratch.deduplicated_admissions;
+refresh materialized view scratch.admissions_form_tidy_json;
+refresh materialized view scratch.admissions_form_exploded;
+
+refresh materialized view scratch.deduplicated_discharges;
+refresh materialized view scratch.scratch.discharges_form_tidy_json;
+refresh materialized view scratch.discharges_form_exploded;
