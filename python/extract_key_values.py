f37c46eb3e15a80275266c1fffdea20734d36030
Daniel Silksmith <daniel.silksmith@johnlewis.co.uk>
2020-04-24 18:26:38 +0100

    generalised input and output variables

--- python/extract_key_values.py
+++ python/extract_key_values.py
@@ -3,37 +3,37 @@
 # These include: MCL, empty lists and list with one element
 # The input is the raw json data and the output is a reformed json (newentrieslist)
 
-def get_key_values(json_raw):
+def get_key_values(data_raw):
     
     # Will store the final list of reformed key-value pairs
-    json_new = []
+    data_new = []
     # n_rows is the number of rows that need to be iterated
-    n_rows = len(json_raw) 
+    n_rows = len(data_raw) 
     # loop to iterate through rows
     for row in range(n_rows):
         # n_col is the number of key value pairs to iterate through in each row 
-        n_col = len(json_raw['entries'][row])
+        n_col = len(data_raw['entries'][row])
         # dict to store all of the key value pairs for each row
         new_entries = {}
         # loop to iterate through key value pairs and add to dict
         for col in range(n_col):
             # branch to extract MCL
-            if len(json_raw['entries'][row][col]['values']) > 1:
+            if len(data_raw['entries'][row][col]['values']) > 1:
                 new_value={}
-                current_value = json_raw['entries'][row][col]['values']
+                current_value = data_raw['entries'][row][col]['values']
                 # code to refactor MCL values
                 for k,v in [(key,d[key]) for d in current_value for key in d]:
                     if k not in new_value: new_value[k]=[v]
                     else: new_value[k].append(v)
-                new_entries[json_raw['entries'][row][col]['key']] = new_value
+                new_entries[data_raw['entries'][row][col]['key']] = new_value
             # branch to cater for empty values
-            elif len(json_raw['entries'][row][col]['values']) == 0:
-                new_entries[json_raw['entries'][row][col]['key']] = json_raw['entries'][row][col]['values']
+            elif len(data_raw['entries'][row][col]['values']) == 0:
+                new_entries[data_raw['entries'][row][col]['key']] = data_raw['entries'][row][col]['values']
             else:
                 # branch to extract single entry values
-                new_entries[json_raw['entries'][row][col]['key']] = json_raw['entries'][row][col]['values'][0]
+                new_entries[data_raw['entries'][row][col]['key']] = data_raw['entries'][row][col]['values'][0]
         # for each row add all the key value pairs
-        json_new.append(new_entries)     
+        data_new.append(new_entries)     
 
-    return json_new
+    return data_new
 
