60ef72aefa48f58fe902895eaed1223495989f5c
Yali <yali@snowplowanalytics.com>
2020-10-02 12:09:22 +0100

    Granting myself permissions on the relevant database tables :-)

--- step_5_access_files/3-grant-usage-on-tables.sql
+++ step_5_access_files/3-grant-usage-on-tables.sql
@@ -1,5 +1,7 @@
 grant usage on schema derived to danielsilksmith;
 grant usage on schema scratch to danielsilksmith;
+grant usage on schema derived to yalisassoon;
+grant usage on schema scratch to yalisassoon;
 
 grant usage on schema derived to deliwe;
 grant usage on schema derived to farahshair;
@@ -8,6 +10,8 @@ grant usage on schema derived to metabase_usr;
 
 grant select on all tables in schema derived to danielsilksmith;
 grant select on all tables in schema scratch to danielsilksmith;
+grant select on all tables in schema derived to yalisassoon;
+grant select on all tables in schema scratch to yalisassoon;
 
 grant select on all tables in schema derived to deliwe;
 grant select on all tables in schema derived to farahshair;
