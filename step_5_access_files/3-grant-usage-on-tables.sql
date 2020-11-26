d04486ebc851903a01df41508f0e352dc407f0c9
mbaradza <mbaradza@hitrac.co.zw>
2020-11-26 23:36:28 +0200

    Add Schema To Convenience views

--- step_5_access_files/3-grant-usage-on-tables.sql
+++ step_5_access_files/3-grant-usage-on-tables.sql
@@ -2,21 +2,16 @@ grant usage on schema derived to danielsilksmith
 grant usage on schema scratch to danielsilksmith
 grant usage on schema derived to yalisassoon 
 grant usage on schema scratch to yalisassoon 
-grant usage on schema derived to neotree 
-grant usage on schema scratch to neotree 
 
 grant usage on schema derived to deliwe
 grant usage on schema derived to farahshair 
 grant usage on schema derived to louisdutoit 
 grant usage on schema derived to metabase_usr 
-grant usage on schema derived to neotree 
 
 grant select on all tables in schema derived to danielsilksmith
 grant select on all tables in schema scratch to danielsilksmith 
 grant select on all tables in schema derived to yalisassoon 
 grant select on all tables in schema scratch to yalisassoon 
-grant select on all tables in schema scratch to neotree 
-grant select on all tables in schema derived to neotree 
 
 grant select on all tables in schema derived to deliwe
 grant select on all tables in schema derived to farahshair
@@ -24,4 +19,3 @@ grant select on all tables in schema derived to louisdutoit
 grant select on all tables in schema derived to metabase_usr
 grant select on all tables in schema derived to powerbi 
 grant select on all tables in schema derived to powerbi_gateway 
-grant select on all tables in schema derived to neotree 
