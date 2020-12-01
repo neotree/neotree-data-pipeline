d5f21897e2a1be21ff4f365a084d827758e17db8
mbaradza <mbaradza@hitrac.co.zw>
2020-12-01 11:18:50 +0200

    Add terminators to step 5

--- step_5_access_files/3-grant-usage-on-tables.sql
+++ step_5_access_files/3-grant-usage-on-tables.sql
@@ -1,21 +1,21 @@
-grant usage on schema derived to danielsilksmith
-grant usage on schema scratch to danielsilksmith
-grant usage on schema derived to yalisassoon 
-grant usage on schema scratch to yalisassoon 
+grant usage on schema derived to danielsilksmith;
+grant usage on schema scratch to danielsilksmith;
+grant usage on schema derived to yalisassoon;
+grant usage on schema scratch to yalisassoon;
 
-grant usage on schema derived to deliwe
-grant usage on schema derived to farahshair 
-grant usage on schema derived to louisdutoit 
-grant usage on schema derived to metabase_usr 
+grant usage on schema derived to deliwe;
+grant usage on schema derived to farahshair; 
+grant usage on schema derived to louisdutoit;
+grant usage on schema derived to metabase_usr;
 
-grant select on all tables in schema derived to danielsilksmith
-grant select on all tables in schema scratch to danielsilksmith 
-grant select on all tables in schema derived to yalisassoon 
-grant select on all tables in schema scratch to yalisassoon 
+grant select on all tables in schema derived to danielsilksmith;
+grant select on all tables in schema scratch to danielsilksmith; 
+grant select on all tables in schema derived to yalisassoon;
+grant select on all tables in schema scratch to yalisassoon;
 
-grant select on all tables in schema derived to deliwe
-grant select on all tables in schema derived to farahshair
-grant select on all tables in schema derived to louisdutoit
-grant select on all tables in schema derived to metabase_usr
-grant select on all tables in schema derived to powerbi 
-grant select on all tables in schema derived to powerbi_gateway 
+grant select on all tables in schema derived to deliwe;
+grant select on all tables in schema derived to farahshair;
+grant select on all tables in schema derived to louisdutoit;
+grant select on all tables in schema derived to metabase_usr;
+grant select on all tables in schema derived to powerbi;
+grant select on all tables in schema derived to powerbi_gateway;
