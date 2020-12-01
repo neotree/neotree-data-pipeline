332e89fc9b6b645fc2fe2bf2476d39147248ba5f
mbaradza <mbaradza@hitrac.co.zw>
2020-12-01 17:55:23 +0200

    Testing Grant Script

--- step_5_access_files/3-grant-usage-on-tables.sql
+++ step_5_access_files/3-grant-usage-on-tables.sql
@@ -1,53 +1,38 @@
-DO $$
+DO $do$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'danielsilksmith') THEN
-        grant usage on schema derived to danielsilksmith AND
-        grant usage on schema scratch to danielsilksmith AND 
-        grant select on all tables in schema derived to danielsilksmith AND
-        grant select on all tables in schema scratch to danielsilksmith
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'danielsilksmith') THEN
+        grant usage on schema derived to danielsilksmith;
+        grant usage on schema scratch to danielsilksmith;
+        grant select on all tables in schema derived to danielsilksmith;
+        grant select on all tables in schema scratch to danielsilksmith;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'yalisassoon') THEN
-        grant usage on schema derived to yalisassoon AND 
-        grant usage on schema scratch to yalisassoon AND
-        grant select on all tables in schema derived to yalisassoon AND
-        grant select on all tables in schema scratch to yalisassoon
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'yalisassoon') THEN
+        grant usage on schema derived to yalisassoon;
+        grant usage on schema scratch to yalisassoon;
+        grant select on all tables in schema derived to yalisassoon;
+        grant select on all tables in schema scratch to yalisassoon;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'deliwe') THEN 
-        grant usage on schema derived to deliwe AND 
-        grant select on all tables in schema derived to deliwe AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'deliwe') THEN
+        grant usage on schema derived to deliwe;
+        grant select on all tables in schema derived to deliwe;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'farahshair') THEN 
-        grant usage on schema derived to farahshair AND 
-        grant select on all tables in schema derived to farahshair AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'farahshair') THEN
+        grant usage on schema derived to farahshair;
+        grant select on all tables in schema derived to farahshair;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'louisdutoit') THEN 
-        grant usage on schema derived to louisdutoit AND 
-        grant select on all tables in schema derived to louisdutoit AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'louisdutoit') THEN
+        grant usage on schema derived to louisdutoit;
+        grant select on all tables in schema derived to louisdutoit;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'metabase_usr') THEN 
-        grant select on all tables in schema derived to metabase_usr AND 
-        grant usage on schema derived to metabase_usr AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'metabase_usr') THEN
+        grant select on all tables in schema derived to metabase_usr;
+        grant usage on schema derived to metabase_usr;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'powerbi') THEN 
-        grant select on all tables in schema derived to powerbi AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'powerbi') THEN
+        grant select on all tables in schema derived to powerbi;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'powerbi_gateway') THEN 
-        grant select on all tables in schema derived to powerbi_gateway AND 
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'powerbi_gateway') THEN
+        grant select on all tables in schema derived to powerbi_gateway;
     END IF;
-END
-$$;
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
+END;
+$do$;
